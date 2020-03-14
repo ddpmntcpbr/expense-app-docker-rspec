9902ede64836bc203e0b8f18ff00b0e07fcd1cfa
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-03-14 14:28:35 +0900

    MOdify environment/production.rb about sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,16 +94,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # mailer setting
-  config.action_mailer.default_url_options = { :host => 'https://safe-thicket-28418.herokuapp.com/'}
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => "heroku.com",
-    :address => "smtp.sendgrid.net",
-    :port => 587,
+  host = 'https://shrouded-reef-29629.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
