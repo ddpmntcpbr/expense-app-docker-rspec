bd9eb249f15a31d6f03536b300f3bfa31624f492
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-03-14 15:27:45 +0900

    Add presence about Rails.env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,7 @@ Rails.application.configure do
 
   # mailer setting
   config.action_mailer.default_url_options = { :host => 'https://shrouded-reef-29629.herokuapp.com/' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
