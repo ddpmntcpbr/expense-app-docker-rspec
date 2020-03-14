becb718b535d7c480231fe3d4e1a1fad68fbea39
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-03-14 15:00:06 +0900

    Modify environment/production.rb about sendgrid config ver2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,17 +94,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # mailer setting
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'https://shrouded-reef-29629.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
-  host = 'https://shrouded-reef-29629.herokuapp.com/'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => "heroku.com",
+    :address => "smtp.sendgrid.net",
+    :port => 587,
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
