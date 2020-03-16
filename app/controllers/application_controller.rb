218fc5e9fcb7a5788bfda71ae0972cfbfbabc06d
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-03-16 21:15:53 +0900

    Apply devise_invitable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name])
+    devise_parameter_sanitizer.permit(:invite, keys: [:email,:last_name,:first_name,:company_id,:approver])
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:password, :password_confirmation, :invitation_token])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
-  
+         :confirmable, invite_for: 2.weeks
+    
   belongs_to :company
 end
